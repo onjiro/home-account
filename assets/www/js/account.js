668b905c6987a7f786cd102f0d32a1079f59d8e3
onjiro <onjiro.mohyahya@gmail.com>
2012-07-08 22:45:40 +0900

    move `Account#save` onto prototype chain

--- assets/www/js/account.js
+++ assets/www/js/account.js
@@ -1,20 +1,19 @@
 this.Account = function(values) {
     values = values || {}
-    // properties
     this.item = values.item;
     this.amount = values.amount || 0;
     this.date = values.date || new Date();
-    // methods
-    this.save = function(tx, onSuccess, onError) {
-        tx.executeSql(
-            'INSERT INTO ACCOUNT (DATE, ITEM, AMOUNT) VALUES (?, ?, ?)',
-            [this.date, this.item, this.amount],
-            function(tx, resultSet) {
-                onSuccess(tx, resultSet.insertId);
-            },
-            onError
-        )
-    };
+}
+
+this.Account.prototype.save = function(tx, onSuccess, onError) {
+    tx.executeSql(
+        'INSERT INTO ACCOUNT (DATE, ITEM, AMOUNT) VALUES (?, ?, ?)',
+        [this.date, this.item, this.amount],
+        function(tx, resultSet) {
+            onSuccess(tx, resultSet.insertId);
+        },
+        onError
+    )
 }
 
 this.Account.find = function(tx, onSuccess, onError) {
