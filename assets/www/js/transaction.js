604022ccd670e29c8bc42d1cedbc4c0443cf85e7
onjiro <onjiro.mohyahya@gmail.com>
2012-08-15 23:15:39 +0900

    Add date and details fields to Transaction

--- assets/www/js/transaction.js
+++ assets/www/js/transaction.js
@@ -1,7 +1,9 @@
 this.Transaction = (function(global) {
     var Constractor = function(values) {
         values = values || {};
+        this.date     = values.date     || new Date();
         this.accounts = values.accounts || [];
+        this.details  = values.details  || "";
     };
     return Constractor;
 })(this);
--- jstest/transaction.test.js
+++ jstest/transaction.test.js
@@ -3,20 +3,33 @@ var sinon = require('sinon');
 var Transaction = require('../assets/www/js/transaction.js').Transaction;
 
 describe('Transaction', function() {
+    
     describe('#initialize', function() {
+        
         it('should be a constractor', function() {
             var target = new Transaction();
             expect(target).to.be.a(Transaction);
         });
+        
         it('should have default values', function() {
             var target = new Transaction();
+            expect(target.date).to.be.a(Date);
             expect(target.accounts).to.be.empty();
+            expect(target.details).to.be("");
         });
+        
         it('should accepts initial values', function() {
-            var target = new Transaction({accounts: ["hoge", "fuga"]});
-            expect(target.accounts).to.have.length(2);
-            expect(target.accounts[0]).to.be("hoge");
-            expect(target.accounts[1]).to.be("fuga");
+            var values = {
+                date    : new Date("2012-04-01"),
+                accounts: ["hoge", "fuga"],
+                details : "details"
+            };
+            
+            var target = new Transaction(values);
+            
+            expect(target.date).to.be(values.date);
+            expect(target.accounts).to.be(values.accounts);
+            expect(target.details).to.be(values.details);
         });
     });
 });
