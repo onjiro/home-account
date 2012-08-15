f31edd4bcd24dea72841a40da115f0e4968e21ba
onjiro <onjiro.mohyahya@gmail.com>
2012-08-15 22:14:56 +0900

    List specs for Transaction's constractor

--- jstest/transaction.test.js
+++ jstest/transaction.test.js
@@ -3,4 +3,9 @@ var sinon = require('sinon');
 var Account = require('../assets/www/js/transaction.js').Account;
 
 describe('Transaction', function() {
+    describe('#initialize', function() {
+        it('should be a constractor');
+        it('should not store accounts unless argument passed');
+        it('should store argument as accounts');
+    });
 });
