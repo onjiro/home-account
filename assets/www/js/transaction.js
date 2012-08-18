ef9a7710f6c418916d01d336e689f8f4289547a3
onjiro <onjiro.mohyahya@gmail.com>
2012-08-18 20:32:04 +0900

    Fix typo, 'Constructor' is correct

--- assets/www/js/transaction.js
+++ assets/www/js/transaction.js
@@ -1,9 +1,9 @@
 this.Transaction = (function(global) {
-    var Constractor = function(values) {
+    var Constructor = function(values) {
         values = values || {};
         this.date     = values.date     || new Date();
         this.accounts = values.accounts || [];
         this.details  = values.details  || "";
     };
-    return Constractor;
+    return Constructor;
 })(this);
