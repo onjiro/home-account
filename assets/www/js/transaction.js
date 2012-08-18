631b92048c7b241ef90be1d0fc920feceb0eb6f1
onjiro <onjiro.mohyahya@gmail.com>
2012-08-18 20:33:05 +0900

    Implements `Transaction#save`

--- assets/www/js/transaction.js
+++ assets/www/js/transaction.js
@@ -5,5 +5,22 @@ this.Transaction = (function(global) {
         this.accounts = values.accounts || [];
         this.details  = values.details  || "";
     };
+    
+    Constructor.prototype.save = function(tx, onSuccess, onError) {
+        // accounts はそれぞれ Accounts テーブルに格納
+        for (var i = 0; i < this.accounts.length; i++) {
+            this.accounts[i].save(tx, null, onError);
+        }
+        // Transactions テーブルに格納
+        tx.executeSql(
+            'INSERT INTO Transactions (date, details) VALUES (?, ?)',
+            [this.date, this.details],
+            function(tx, resultSet) {
+                if (onSuccess) { onSuccess(tx, resultSet.insertId); };
+            },
+            onError
+        );
+    };
+    
     return Constructor;
 })(this);
--- jstest/transaction.test.js
+++ jstest/transaction.test.js
@@ -34,4 +34,36 @@ describe('Transaction', function() {
             expect(target.details).to.be(values.details);
         });
     });
+    
+    describe('#save', function() {
+        var txMock;
+        beforeEach(function() {
+            txMock = {};
+            txMock.executeSql = sinon.spy.create(function(sql, def, onSuccess, onError) {
+                onSuccess(this, txMock.executeSql.resultSet);
+            });
+            txMock.executeSql.resultSet = {
+                insertId: 'the insertId',
+                rowsAffected: 1,
+                rows: undefined,
+            };
+        });
+        
+        it ('should be a function', function() {
+            expect(target.save).to.be.a('function');
+        });
+        it ('should pass transaction and insertId for callback', function() {
+            var success = function(tx, insertId) {
+                expect(tx).to.eql(txMock);
+                expect(insertId).to.be('the insertId');
+            };
+            target.save(txMock, success);
+            expect(txMock.executeSql.called).to.be.ok();
+        });
+        it ('should allow no success-callback passed', function() {
+            target.save(txMock);
+            expect(txMock.executeSql.called).to.be.ok();
+        })
+    });
+
 });
