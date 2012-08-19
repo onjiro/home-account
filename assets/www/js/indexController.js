0adeb64a5222ddfba364d426f50b8a34b6274a3a
onjiro <onjiro.mohyahya@gmail.com>
2012-08-19 23:46:40 +0900

    Fix month for display
    Date#getMonth needs `+1` for display.
    It start with 0.

--- assets/www/js/indexController.js
+++ assets/www/js/indexController.js
@@ -98,7 +98,7 @@ $(function() {
     var addToTransactionHistory = function($target, transactions) {
         var format = function(date) {
             return date.getFullYear()
-                + '/' + ('0' + date.getMonth()).slice(-2)
+                + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
                 + '/' + ('0' + date.getDate()).slice(-2)
                 + ' ' + ('0' + date.getHours()).slice(-2)
                 + ':' + ('0' + date.getMinutes()).slice(-2);
