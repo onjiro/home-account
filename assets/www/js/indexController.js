536c92e5d8ce9307e22b3f4f8545a75fc6f0f331
onjiro <onjiro.mohyahya@gmail.com>
2012-08-23 22:59:54 +0900

    Divide database initialization code from indexController.js to initialize.js

--- assets/www/js/indexController.js
+++ assets/www/js/indexController.js
@@ -1,24 +1,4 @@
 $(function() {
-    var db = openDatabase('home-account', '', 'home account', 100000);
-    var m = new Migrator(db);
-    m.migration(1, function(tx) {
-        Account.init(db);
-    });
-    m.migration(2, function(tx) {
-        Transaction.init(db);
-    });
-    m.migration(3, function(tx) {
-        var sql = 'SELECT DISTINCT date FROM Accounts ';
-        tx.executeSql(sql, [], function(tx, resultSet) {
-            for (var i = 0; i < resultSet.rows.length; i++) {
-                new Transaction({
-                    date: resultSet.rows.item(i).date,
-                }).save(tx);
-            }
-        });
-    });
-    m.doIt();
-    
     // タブの動作
     $('.js-tab a').click(function(e) {
         e.preventDefault();
