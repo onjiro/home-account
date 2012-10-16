921a369e225ffc6a06f663fce2a14b8a363d33ee
onjiro <onjiro.mohyahya@gmail.com>
2012-10-16 22:12:16 +0900

    Add Account::total to calculate total amount for all accounts

--- assets/www/js/account.js
+++ assets/www/js/account.js
@@ -28,5 +28,23 @@ this.Account = (function(global) {
         }, onError)
     }
 
+    Constructor.total = function(tx, onSuccess, onError) {
+        // TODO 現在の日付を条件に追加するには date の保存形式の変更が必要
+        tx.executeSql([
+            'SELECT',
+            '  item,',
+            '  type,',
+            '  sum(amount)',
+            'FROM',
+            '  Account',
+            'GROUP BY',
+            '  item,',
+            '  type'
+        ].join(' '), [
+            // no query data given
+        ], function(tx, resultSet) {
+            if (onSuccess) { onSuccess(tx, resultSet); };
+        }, onError);
+    }
     return Constructor;
 })(this);
