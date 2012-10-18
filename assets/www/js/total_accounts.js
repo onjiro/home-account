b7810585be87356584c0d2f59847e29fedad5ab9
onjiro <onjiro.mohyahya@gmail.com>
2012-10-18 22:47:12 +0900

    Make temporaly implementation for `TotalAccounts#makeInventory`

--- assets/www/js/total_accounts.js
+++ assets/www/js/total_accounts.js
@@ -8,9 +8,37 @@ this.TotalAccounts = (function(global){
     
     /**
      * 棚卸登録を行います。
+     * @param tx DatabaseTransaction
      */
-    TotalAccounts.prototype.makeInventory = function() {
-        // TODO
+    TotalAccounts.prototype.makeInventory = function(tx, success, err) {
+        var _this = this;
+        TotalAccounts.select(this.item, tx, function(current) {
+            var now = new Date()
+            , difference = {
+                date: now,
+                item: _this.item,
+                amount: (_this.type === current.type)
+                    ? _this.amount - current.amount
+                    : _this.amount + current.amount,
+                type: (difference.amount < 0)
+                    ? (_this.type === 'credit') ? 'debit': 'credit'
+                    : _this.type
+            }
+            new Transaction({
+                date: now,
+                accounts: [
+                    new Account(difference),
+                    new Account({
+                        date: now,
+                        item: '棚卸差額',
+                        amount: difference.amount,
+                        type: (difference.type === 'credit') ? 'debit': credit
+                    })
+                ]
+            }).save(tx, success, err);
+        }, err);
     }
+
+    // TODO TotalAccounts::select
     return TotalAccounts;
 })(this);
