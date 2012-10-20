acb59af2e4473c238abd113c838e7c7df1519288
onjiro <onjiro.mohyahya@gmail.com>
2012-10-20 23:20:06 +0900

    Implement `TransactionHistoryView#prepend`

--- assets/www/js/transactionHistoryView.js
+++ assets/www/js/transactionHistoryView.js
@@ -1,13 +1,55 @@
-this.TransactionHistoryView = (function() {
-    var TransactionHistoryView = function() {
+this.TransactionHistoryView = (function(global) {
+    /**
+     * @parameter $parent TransactionHistory の追加対象 jQuery エレメント
+     */
+    var TransactionHistoryView = function($parent) {
+        this.$parent = $parent;
     }
-    , _this = TransactionHistoryView;
+    , _this = TransactionHistoryView
+    , format = function(date) {
+        return date.getFullYear()
+            + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
+            + '/' + ('0' + date.getDate()).slice(-2)
+            + ' ' + ('0' + date.getHours()).slice(-2)
+            + ':' + ('0' + date.getMinutes()).slice(-2);
+    }
+    , formatToTableRow = function(transaction) {
+        var item = ''
+        , amount = 0
+        , creditItems = []
+        , accounts = transaction.accounts;
+        for (var i = 0; i < accounts.length; i++) {
+            switch (accounts[i].type) {
+            case 'debit':
+                item += (item === '') ? '': ', ';
+                item += accounts[i].item;
+                amount += accounts[i].amount;
+                break;
+            case 'credit':
+                creditItems.push(accounts[i].item);
+                break;
+            }
+        }
+        return [
+            '<tr data-transaction-id="' + transaction.rowid + '">',
+            '  <td>' + format(transaction.date) + '</td>',
+            '  <td>' + item + '</td>',
+            '  <td><span class="label">' + creditItems + '</span></td>',
+            '  <td style="text-align: right;">' + amount + '</td>',
+            '  <td>' + transaction.details + '</td>',
+            '</tr>'
+        ].join('\n');
+    };
 
     /**
      * 表示する要素を追加します。
+     * @parameter transaction 追加対象Transactionインスタンス
      */
-    _this.prototype.prepend = function() {
-        // TODO
+    _this.prototype.prepend = function(transaction) {
+        this.$parent.prepend(formatToTableRow(transaction))
+            .children(':first-child')
+            .hide()
+            .fadeIn();
     }
     return TransactionHistoryView;
 })(this);
--- jstest/transactionHistoryView.test.js
+++ jstest/transactionHistoryView.test.js
@@ -1,11 +1,19 @@
 var expect = require('expect.js')
 , sinon = require('sinon')
+, Account = require('../assets/www/js/account.js').Account
+, Transaction = require('../assets/www/js/transaction.js').Transaction
 , TransactionHistoryView = require('../assets/www/js/transactionHistoryView.js').TransactionHistoryView;
 
 describe('TransactionHistoryView', function() {
-    var target;
+    var target, $parent;
     beforeEach(function() {
-        target = new TransactionHistoryView();
+        $parent = {
+            prepend: sinon.spy.create(function() { return this }),
+            children: sinon.spy.create(function() { return this }),
+            hide: sinon.spy.create(function() { return this }),
+            fadeIn: sinon.spy.create(function() { return this })
+        };
+        target = new TransactionHistoryView($parent);
     });
     describe('#initialize', function() {
         it('should be a function', function() {
@@ -16,6 +24,38 @@ describe('TransactionHistoryView', function() {
     describe('#prepend', function() {
         it('should be a function', function() {
             expect(target.prepend).be.a(Function);
+        });
+        it('should call `$parent.prepend` once', function() {
+            target.prepend(new Transaction());
+            expect($parent.prepend.callCount).be(1);
+        });
+        it('should prepend new dom element to $parent', function() {
+            target.prepend(new Transaction({
+                rowid: 0,
+                date: new Date('2012/04/30 12:34:56'),
+                details: 'details',
+                accounts: [
+                    new Account({
+                        item: 'debit item',
+                        type: 'debit',
+                        amount: 1234567
+                    }),
+                    new Account({
+                        item: 'credit item',
+                        type: 'credit',
+                        amount: 1234567
+                    })
+                ]
+            }));
+            expect($parent.prepend.args[0][0]).to.be.equal([
+                '<tr data-transaction-id="0">',
+                '  <td>2012/04/30 12:34</td>',
+                '  <td>debit item</td>',
+                '  <td><span class="label">credit item</span></td>',
+                '  <td style="text-align: right;">1234567</td>',
+                '  <td>details</td>',
+                '</tr>'
+            ].join('\n'));
         });
     });
 });
