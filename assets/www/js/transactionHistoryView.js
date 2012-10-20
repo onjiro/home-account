88c5fe85c9028f528b639e62c76f6f13cfc214ea
onjiro <onjiro.mohyahya@gmail.com>
2012-10-20 22:06:19 +0900

    add new function `TransactionHistoryView#append`

--- assets/www/js/transactionHistoryView.js
+++ assets/www/js/transactionHistoryView.js
@@ -1,5 +1,13 @@
 this.TransactionHistoryView = (function() {
     var TransactionHistoryView = function() {
     }
+    , _this = TransactionHistoryView;
+
+    /**
+     * 表示する要素を追加します。
+     */
+    _this.prototype.append = function() {
+        // TODO
+    }
     return TransactionHistoryView;
 })(this);
--- jstest/transactionHistoryView.test.js
+++ jstest/transactionHistoryView.test.js
@@ -3,9 +3,19 @@ var expect = require('expect.js')
 , TransactionHistoryView = require('../assets/www/js/transactionHistoryView.js').TransactionHistoryView;
 
 describe('TransactionHistoryView', function() {
+    var target;
+    beforeEach(function() {
+        target = new TransactionHistoryView();
+    });
     describe('#initialize', function() {
         it('should be a function', function() {
             expect(TransactionHistoryView).be.a(Function);
+        });
+    });
+
+    describe('#append', function() {
+        it('should be a function', function() {
+            expect(target.append).be.a(Function);
         });
     });
 });
