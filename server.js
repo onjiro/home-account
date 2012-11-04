d8f0c8f6fb20b994afab5751992e9eae938648ba
onjiro <onjiro.mohyahya@gmail.com>
2012-11-04 22:10:35 +0900

    Add file hosting server script for development

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+, start = function(app, options) {
+    var options = options || {}
+    , port = options.port;
+    app.use(express.static(__dirname + '/assets/www'));
+
+    app.listen(port);
+    console.log('server start at localhost:' + port);
+};
+start(express(), {
+    port: process.env.PORT || 8000
+});
