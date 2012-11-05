ab736fbee4f4e5cbeb76dfd5affa375d49ed885f
onjiro <onjiro.mohyahya@gmail.com>
2012-11-06 00:04:21 +0900

    Remove global leak detected from mocha at test

--- jstest/transactionHistoryView.test.js
+++ jstest/transactionHistoryView.test.js
@@ -8,17 +8,14 @@ if (require) {
 }
 
 describe('TransactionHistoryView', function() {
-    var target, $parent;
-    beforeEach(function() {
-        target = new TransactionHistoryView($parent),
-        $parent = {
-            prepend: sinon.spy.create(function() { return this }),
-            append: sinon.spy.create(function() { return this }),
-            children: sinon.spy.create(function() { return this }),
-            hide: sinon.spy.create(function() { return this }),
-            fadeIn: sinon.spy.create(function() { return this })
-        };
-    });
+    var target = new TransactionHistoryView($parent),
+    $parent = {
+        prepend: sinon.spy.create(function() { return this }),
+        append: sinon.spy.create(function() { return this }),
+        children: sinon.spy.create(function() { return this }),
+        hide: sinon.spy.create(function() { return this }),
+        fadeIn: sinon.spy.create(function() { return this })
+    };
 
     describe('#initialize', function() {
         it('should be a function', function() {
