a61e56271e23d7904c64ebd5c62b43d9bf7f3c07
onjiro <onjiro.mohyahya@gmail.com>
2012-11-09 22:19:43 +0900

    Show month and date only

--- assets/www/js/transactionHistoryView.js
+++ assets/www/js/transactionHistoryView.js
@@ -20,11 +20,7 @@ this.TransactionHistoryView = (function(global) {
         }
     })
     , format = function(date) {
-        return date.getFullYear()
-            + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
-            + '/' + ('0' + date.getDate()).slice(-2)
-            + ' ' + ('0' + date.getHours()).slice(-2)
-            + ':' + ('0' + date.getMinutes()).slice(-2);
+        return (date.getMonth() + 1) + '/' + date.getDate();
     }
     , formatToTableRow = function(transaction) {
         var i
