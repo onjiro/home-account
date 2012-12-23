d3bef5e6c729dcaa521897a13b5446ddd449bc4a
onjiro <onjiro.mohyahya@gmail.com>
2012-12-23 21:03:00 +0900

    Extract bind.js

--- phantomTestRunner.js
+++ phantomTestRunner.js
@@ -3,29 +3,7 @@ var system = require('system')
 , runner;
 if (system.args.length < 2) { console.log('Usage: ' + system.args[0] + ' [TestScript...]'); phantom.exit(1); }
 
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
- 
-    var aArgs = Array.prototype.slice.call(arguments, 1), 
-        fToBind = this, 
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP && oThis
-                                 ? this
-                                 : oThis,
-                               aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
- 
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
- 
-    return fBound;
-  };
-}
+phantom.injectJs('./phantomjslib/bind.js');
 
 // setup mocha
 phantom.injectJs('./node_modules/mocha/mocha.js');
