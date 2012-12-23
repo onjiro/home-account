5a9978cb174832478e3fc0056e011d004d33a468
onjiro <onjiro.mohyahya@gmail.com>
2012-12-23 20:50:22 +0900

    Fix formatting on error

--- phantomTestRunner.js
+++ phantomTestRunner.js
@@ -305,6 +305,8 @@ var formatRegExp = /%[sdj%]/g
 };
 console._log = console.log;
 console.log = function() {console._log(format.apply(this, arguments))};
+console._error = console.error;
+console.error = function() {console._error(format.apply(this, arguments))};
 process.stdout.write = function() {
     console.log.apply(this, arguments);
 };
