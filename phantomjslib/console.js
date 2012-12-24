4b02f866859aea42fff312cfc5438bb29665a986
onjiro <onjiro.mohyahya@gmail.com>
2012-12-24 16:13:08 +0900

    Remove test using phantomjs with mocha

--- phantomjslib/console.js
+++
@@ -1,7 +0,0 @@
-console._log = console.log;
-console.log = function() {console._log(format.apply(this, arguments))};
-console._error = console.error;
-console.error = function() {console._error(format.apply(this, arguments))};
-process.stdout.write = function() {
-    console.log.apply(this, arguments);
-};
--- phantomjstest/index.html.test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('./phantomjslib/expect-0.2.0.js');
-describe('index.html', function() {
-    it('can be opened', function() {
-        page.open('./assets/www/index.html', function(status) {
-            expect(status).to.be('success');
-        });
-    });
-});
-
