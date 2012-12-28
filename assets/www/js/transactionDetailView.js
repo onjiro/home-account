e67beed51b845fbcc42c6791c98bb50f6390c095
onjiro <onjiro.mohyahya@gmail.com>
2012-12-28 22:44:37 +0900

    Remove transaction detail when margin clicked

--- assets/www/js/transactionDetailView.js
+++ assets/www/js/transactionDetailView.js
@@ -1,5 +1,10 @@
 this.TransactionDetailView = (function(global){
     return Backbone.View.extend({
+        events: {
+            'click .history-detail': function(e) {
+                if (e.toElement.className === 'history-detail') this.remove();
+            },
+        },
         initialize: function() {
             var template = _.template($('#history-detail').html());
             $('body').append(this.$el.append(template(this.model.attributes)));
