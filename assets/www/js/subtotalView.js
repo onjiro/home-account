c19b444e4d2c6acd59aa67874a2ab56acaae46b7
onjiro <onjiro.mohyahya@gmail.com>
2013-01-18 23:12:36 +0900

    Re-render on reset collection

--- assets/www/js/subtotalView.js
+++ assets/www/js/subtotalView.js
@@ -1,28 +1,23 @@
 var SubTotalView = (function(global) {
     return Backbone.View.extend({
+        collection: new Backbone.Collection(),
         events: {
             'change input': function(e) {
-                var start = this.$el.find('input[name="start"]').val()
-                ,     end = this.$el.find('input[name="end"]').val()
-                ,  $tbody
-                ,   _this = this;
+                var    start = this.$el.find('input[name="start"]').val()
+                ,        end = this.$el.find('input[name="end"]').val()
+                , collection = this.collection;
                 if (!start || !end) return;
 
                 this.$el
                     .find('.loading').show()
                     .siblings().not('input');
-                ($tbody = this.$el.find('tbody')).empty();
+                this.$tbody.empty();
                 db.transaction(function(tx) {
                     TotalAccount.select({
                         startDate: new Date(start),
                         endDate:   new Date(end),
                     }, tx, function(tx, totals) {
-                        _.each(totals, function(subTotal){
-                            $tbody.append(_this.template(subTotal.attributes));
-                        });
-                        _this.$el
-                            .find('.subtotals').show()
-                            .siblings().not('input').hide();
+                        collection.reset(totals);
                     });
                 }, function(err) {
                     alert('something failed on query TotalAccounts.\n' + err.message);
@@ -30,7 +25,16 @@ var SubTotalView = (function(global) {
             },
         },
         initialize: function() {
+            this.collection.on('reset', function(models, options) {
+                _.each(models.models, function(subTotal){
+                    this.$tbody.append(this.template(subTotal.attributes));
+                }, this);
+                this.$el
+                    .find('.subtotals').show()
+                    .siblings().not('input').hide();
+            }, this);
             this.template = _.template($('#sub-total-template').html());
+            this.$tbody = this.$el.find('tbody');
         },
     });
 })(this);
