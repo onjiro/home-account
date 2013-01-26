51b543475acf11156dac3bb58109eabaa6b960ac
onjiro <onjiro.mohyahya@gmail.com>
2013-01-26 13:35:21 +0900

    Support entry account item from select box

--- assets/www/js/appView.js
+++ assets/www/js/appView.js
@@ -1,5 +1,8 @@
 this.AppView = (function(global) {
     return Backbone.View.extend({
+        events: {
+            'change [name="item-in-selection"],[name="opposite-item-in-selection"]': function(e) { this.onSelectItem($(e.srcElement)); },
+        },
         initialize: function(options) {
             var _this = this
             , selectionTemplate = _.template($('#selection-template').html());
@@ -15,7 +18,13 @@ this.AppView = (function(global) {
                         item: model.get('name'),
                     }));
                 });
+                _this.onSelectItem($select);
             });
+        },
+        onSelectItem: function($selection) {
+            $selection
+                .siblings('[name="item"], [name="opposite-item"]')
+                .val($selection.val());
         },
     });
 })(this);
