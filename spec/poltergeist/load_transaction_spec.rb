d3a66764810c57bc865d2db813b9f09c5c222b31
onjiro <onjiro.mohyahya@gmail.com>
2013-02-04 22:54:11 +0900

    Add pending example '勘定科目をセレクトボックスから選択してTransactionを追加できること'
    poltergeistでjQueryのselectイベントが発行されないバグのため当テストは現状pendingとする。
    https://github.com/jonleighton/poltergeist/issues/204

--- spec/poltergeist/load_transaction_spec.rb
+++ spec/poltergeist/load_transaction_spec.rb
@@ -55,5 +55,30 @@ describe 'transaction history', :type => :feature do
         end
       end
     end
+
+    it '勘定科目をセレクトボックスから選択してTransactionを追加できること' do
+      pending 'poltergeistでjQueryのselectイベントが発行されないため https://github.com/jonleighton/poltergeist/issues/204'
+
+      within('form#account-entry') do
+        fill_in 'amount', :with => '40'
+        select '現金'   , :from => 'item-in-selection'
+        select '食費'   , :from => 'opposite-item-in-selection'
+      end
+      find_button('決定').trigger('click')
+
+      within('#history tbody') do
+        page.should have_selector('tr', :count => 2)
+        within('tr:first-child') do
+          find('td:nth-child(1)').should have_content(entry_time)
+          find('td:nth-child(2)').should have_content('食費')
+          find('td:nth-child(3)').should have_content('120')
+        end
+      end
+      within('form#account-entry') do
+        find_field('amount').value.should        eq('')
+        find_field('item').value.should          eq('')
+        find_field('opposite-item').value.should eq('')
+      end
+    end
   end
 end
