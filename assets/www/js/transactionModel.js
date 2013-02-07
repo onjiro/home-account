06f121feebc269c25d3f38a2177f928237825a5f
onjiro <onjiro.mohyahya@gmail.com>
2013-02-07 14:34:00 +0900

    Move `Transaction#find` into a part of `TransactionList#sync`

--- assets/www/js/transactionModel.js
+++ assets/www/js/transactionModel.js
@@ -74,47 +74,5 @@ this.Transaction = (function(global) {
                 break;
             }
         },
-    }, {
-        // class properties
-        find: function(tx, option, onSuccess, onError) {
-            var sql, where = [];
-            if (option.from) {
-                where.push(' Transactions.date >= ' + option.from.getTime());
-            }
-            sql = ''
-                + 'SELECT '
-                +   'Transactions.rowid as id,'
-                +   'Transactions.date as date,'
-                +   'Transactions.details as details,'
-                +   'Transactions.rowid as transactionId,'
-                +   'AccountItems.name as item,'
-                +   'Accounts.amount as amount,'
-                +   'Accounts.type as type '
-                + 'FROM '
-                +   'Transactions '
-                +   'INNER JOIN Accounts '
-                +     'ON Transactions.rowid = Accounts.transactionId '
-                +   'INNER JOIN AccountItems '
-                +     'ON Accounts.itemId = AccountItems.rowid '
-                + ((where.length === 0) ? '': 'WHERE' + where.join(',') + ' ')
-                + 'ORDER BY '
-                +   'Transactions.rowid ';
-            tx.executeSql(sql, [], function(tx, resultSet) {
-                var transactions, resultArray = [];
-                for (var i = 0; i < resultSet.rows.length; i++) {
-                    resultArray.push(resultSet.rows.item(i));
-                }
-                transactions = _.chain(resultArray)
-                    .groupBy('id')
-                    .map(function(group) {
-                        return new Transaction(group[0])
-                            .set('accounts', _.map(group, function(one) {
-                                return new Account(one);
-                            }));
-                    })
-                    .value();
-                onSuccess(tx, transactions);
-            }, onError);
-        }
     });
 })(this);
