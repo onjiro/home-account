89e431278c5efc5263f94a93113649c6adc31145
onjiro <onjiro.mohyahya@gmail.com>
2013-03-03 00:30:18 +0900

    Extract `CommonlyUseAccountItemList` into new file

--- assets/www/js/view.commonlyUseAccount.js
+++ assets/www/js/view.commonlyUseAccount.js
@@ -1,35 +1,3 @@
-var CommonlyUseAccountItemList = (function(global) {
-    return Backbone.Collection.extend({
-        model: AccountItem,
-        sqls: {
-            read: ''
-                + 'SELECT '
-                +   'AccountItems.rowid as id,'
-                +   'AccountItems.name as name,'
-                +   'AccountItemClassifications.name as classification '
-                + 'FROM AccountItems '
-                +   'INNER JOIN AccountItemClassifications '
-                +     'ON AccountItems.classificationId = AccountItemClassifications.rowid '
-                +   'INNER join ('
-                +     'SELECT '
-                +       'itemId,'
-                +       'count(*) as count '
-                +     'FROM Accounts '
-                +     'WHERE '
-                +       'type = ? '
-                +     'GROUP BY '
-                +       'itemId '
-                +   ') AccountCounts ON AccountItems.rowid = AccountCounts.itemId '
-                + 'ORDER BY AccountCounts.count DESC '
-                + 'LIMIT ?',
-        },
-        placeholders: {
-            read: ['side', 'limit'],
-        },
-    });
-})();
-
-// 初期化処理
 $(function() {
     window.CommonlyUseAccountView = Backbone.View.extend({
         template: _.template($('#template-commonly-used-account').html()),
