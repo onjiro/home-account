cd6896435501e60f911b20278f2662c0e4537102
onjiro <onjiro.mohyahya@gmail.com>
2013-03-30 17:33:25 +0900

    Introduction require.js

---
+++ assets/www/js/main.js
@@ -0,0 +1,155 @@
+require([
+    'jquery',
+    'underscore',
+], function($, _) {
+    $(function() {
+        Account.items = new AccountItemList();
+        // bootstrap の Alert div のテンプレート
+        var alertTemplate = _.template($('#alert-template').html())
+        , $history = $('#history')
+        , currentTransactions = new TransactionList()
+        , historyView = new TransactionHistoryView({
+            el: '#history',
+            collection: currentTransactions
+        })
+        , appView = new AppView({
+            el: document,
+            collection: currentTransactions,
+            accountItems: Account.items,
+        })
+        , totalAccounts = new Backbone.Collection()
+        , inventoryTabView = new InventoryTabView({
+            el: '#inventory-tab',
+            collection: totalAccounts,
+        })
+        , subtotalView = new SubTotalView({
+            el: '#subtotal-tab',
+        })
+        , calculateDaysAgo = function(targetDate, days) {
+            var aWeekAgo = new Date(targetDate.getTime() - days * 24 * 3600 * 1000);
+            aWeekAgo.setHours(0);
+            aWeekAgo.setMinutes(0);
+            aWeekAgo.setSeconds(0);
+            aWeekAgo.setMilliseconds(0);
+            return aWeekAgo;
+        };
+        window.withSeparators = function(amount) {
+            var num = new String(amount).replace(/,/g, '');
+            while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
+            return num;
+        };
+        Backbone.sync.db = db;
+
+        // datepickerの設定
+        $('input[type="date"], .datepicker').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+
+        // 初期ロード時に AccountItems を読み込む
+        db.transaction(function(tx) {
+            Account.items.fetch({tx: tx});
+        });
+
+        // 初期ロード時に Transaction を読み込む
+        currentTransactions.fetch({ from: calculateDaysAgo(new Date(), 7) });
+
+        // 初期ロード時に TotalAccount を読み込む
+        db.transaction(function(tx) {
+            TotalAccount.select({date: new Date()}, tx, function(tx, accounts) {
+                totalAccounts.add(accounts);
+
+                // backboned model 間の依存 listen 関係を定義
+                currentTransactions.on('add change remove', function(model, collection, option) {
+                    db.transaction(function(tx) {
+                        TotalAccount.select({date: new Date()}, tx, function(tx, accounts) {
+                            totalAccounts.reset(accounts);
+                        });
+                    });
+                });
+            }, function(err) {
+                alert('something failed on query TotalAccounts.\n' + err.message);
+            });
+        }, function(err) {
+            alert('something failed while accessing Accounts.\n' + err.message);
+        });
+
+        // 棚卸時、選択された科目を科目欄に入力qし、フォームに移動する
+        $(document).on('click', '#inventory-tab tbody a', function(e) {
+            $('#inventory-entry [name="item"]').val($(this).closest('tr').data('item'));
+        });
+
+        // 棚卸登録
+        $(document).on('submit', '#inventory-entry', function(e) {
+            var form = this;
+            db.transaction(function(tx) {
+                new TotalAccount({
+                    amount: $('[name="amount"]', form).val(),
+                    item:   $('[name="item"]', form).val(),
+                    type:   $('[name="account-type"]:checked', form).val()
+                }).makeInventory(tx, function(tx, total, newTransaction) {
+                    currentTransactions.add(newTransaction, {at: 0, newest: true});
+                    form.reset();
+                }, function(err) {
+                    alert('something failed while make an inventory.\n' + err.message);
+                });
+            });
+            return false;
+        });
+
+        // エントリータブのビュー
+        new EntryTabView({
+            el: $('#entry-tab'),
+            collection: currentTransactions,
+            alertTemplate: alertTemplate,
+        });
+
+        // 支出登録のビューモデル
+        var accounts = {
+            debit:  new Backbone.Model({ accountItem: '' }),
+            credit: new Backbone.Model({ accountItem: '' }),
+        }
+        var AccountItemEntryView = Backbone.View.extend({
+            initialize: function() {
+                this.model.on('change', this.render, this);
+                this.render();
+            },
+            render: function() {
+                this.$('input').val(this.model.get('accountItem'));
+                return this;
+            },
+        });
+        new AccountItemEntryView({
+            el: $('.control-group.item'),
+            model: accounts.debit,
+        });
+        new AccountItemEntryView({
+            el: $('.control-group.opposite-item'),
+            model: accounts.credit,
+        });
+
+        // よく使う勘定科目の表示領域
+        var commonlyUseAccounts = {
+            debit:  new CommonlyUseAccountItemList(),
+            credit: new CommonlyUseAccountItemList(),
+        }
+        new CommonlyUseAccountAreaView({
+            el: $('.debit.commonly-use'),
+            model: accounts.debit,
+            collection: commonlyUseAccounts.debit,
+        });
+        new CommonlyUseAccountAreaView({
+            el: $('.credit.commonly-use'),
+            model: accounts.credit,
+            collection: commonlyUseAccounts.credit,
+        });
+
+        commonlyUseAccounts.debit.fetch({
+            side: 'debit',
+            limit: 3,
+        });
+        commonlyUseAccounts.credit.fetch({
+            side: 'credit',
+            limit: 3,
+        });
+    });
+});
---
+++ assets/www/js/requireConfig.js
@@ -0,0 +1,37 @@
+var require = {
+    baseUrl: 'js/',
+    deps: [
+        'bootstrap',
+        'jquery',
+        'jquery-ui',
+        'underscore',
+        'backbone',
+        'backbone-websql',
+    ],
+    paths: {
+        'bootstrap': 'bootstrap.min',
+        'backbone': 'backbone-min',
+        'backbone-websql': 'backboneWebSql',
+        'underscore': 'underscore-min',
+        'jquery': 'jquery-1.7.2.min',
+        'jquery-ui': 'jquery-ui-1.10.0.custom',
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone',
+        },
+        'backbone-websql': {
+            deps: ['backbone'],
+        },
+        'underscore': {
+            exports: '_',
+        },
+        'jquery': {
+            exports: '$'
+        },
+        'jquery-ui': {
+            deps: ['jquery']
+        },
+    },
+}
